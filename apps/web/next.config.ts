778fad816422dca68fe10b192c935638cb25e62b
Musanna Al Akil <43068505+Musanna-al-akil@users.noreply.github.com>
2025-01-19 19:40:04 +0600

    fix: testing api proxy in production

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
     /* config options here */
     async rewrites() {
         return [
             {
                 source: "/api/:path*",
-                destination: `https://next-hono-turborepo-api.vercel.app
-/api/:path*`,
+                destination: isProduction
+                    ? "https://next-hono-turborepo-api.vercel.app/api/:path*"
+                    : "http://localhost:3001/api/:path*",
             },
         ];
     },
