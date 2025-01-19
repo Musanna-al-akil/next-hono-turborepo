c8f8eb5413474df3e5bc30abf6aa33a40c0e1a5e
Musanna Al Akil <43068505+Musanna-al-akil@users.noreply.github.com>
2025-01-19 20:08:56 +0600

    test--

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
     /* config options here */
     async rewrites() {
